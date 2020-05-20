fd3a7b3e1627b69a73e9a82066ff4396959d5eef
Sandes <sandeshpathak0112@gmail.com>
2020-05-20 14:16:43 +1000

    initial setup

---
+++ js/checkVisible.js
@@ -0,0 +1,10 @@
+function checkVisible( elm, eval ) {
+    eval = eval || "object visible";
+    var viewportHeight = $(window).height(), // Viewport Height
+        scrolltop = $(window).scrollTop(), // Scroll Top
+        y = $(elm).offset().top,
+        elementHeight = $(elm).height();   
+
+    if (eval == "object visible") return ((y < (viewportHeight + scrolltop)) && (y > (scrolltop - elementHeight)));
+    if (eval == "above") return ((y < (viewportHeight + scrolltop)));
+}

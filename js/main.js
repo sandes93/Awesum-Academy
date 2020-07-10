74b9580d90623716cecc98a8c527839847ace64f
Sandes <sandeshpathak0112@gmail.com>
2020-07-10 16:55:32 +1000

    fixed css

--- js/main.js
+++ js/main.js
@@ -1,24 +1,9 @@
+'use strict';
 
 function main() {
 
 (function () {
-   'use strict';
 
-	// Hide .navbar first
-	// $(".navbar").hide();
-	
-	// // Fade in .navbar
-	// $(function () {
-	// 	$(window).scroll(function () {
- //            // set distance user needs to scroll before we fadeIn navbar
-	// 		if ($(this).scrollTop() > 200) {
-	// 			$('.navbar').fadeIn();
-	// 		} else {
-	// 			$('.navbar').fadeOut();
-	// 		}
-	// 	});
-	
-	// });
 	
 	// Preloader */
 	  	$(window).load(function() {
